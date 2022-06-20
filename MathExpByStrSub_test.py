774dee04e09dde597e9461d460cb914487b8ee6f
jjjustin0717 <jjjustin0717@github.com>
2022-06-20 21:26:55 +0800

    Update MathExpByStrSub_test.py

--- MathExpByStrSub_test.py
+++ MathExpByStrSub_test.py
@@ -1,5 +1,8 @@
 import unittest
 
+from hypothesis import given
+import hypothesis.strategies as st
+
 from MathExpByStrSub import MathExpByStrSub
 
 
@@ -84,11 +87,51 @@ class TestMathExpByStrSub(unittest.TestCase):
 
         mathExp1 = MathExpByStrSub('foo(x)+2+2*cos(0)/2-y')
         mathExp1.to_rpn()
-        self.assertEqual(mathExp1.evaluate(x=2, y=3, foo=lambda x: x*42), 84.0)
+        self.assertEqual(mathExp1.evaluate(x=2, y=3, foo=lambda x: x * 42), 84.0)
 
         mathExp2 = MathExpByStrSub('a + 2 - sin(0.3) * (b - c)')
         mathExp2.to_rpn()
         self.assertEqual(round(mathExp2.evaluate(a=10, b=20, c=30), 1), 15.0)
+
+    def test_What_happens1(self):
+        try:
+            mathExp = MathExpByStrSub('a + 2 - sin(0.3) * (b - c) / 0')
+            mathExp.to_rpn()
+            mathExp.evaluate(a=10, b=20, c=30)
+        except ZeroDivisionError:
+            print(repr(ZeroDivisionError))
+
+    def test_What_happens2(self):
+        try:
+            mathExp = MathExpByStrSub('a + 2 - log(0,0) * (b - c)')
+            mathExp.to_rpn()
+            mathExp.evaluate(a=10, b=20, c=30)
+        except ValueError:
+            print(repr(ValueError))
+
+    @given(st.integers())
+    def test_Property_based(self, a):
+        mathExp = MathExpByStrSub('a-a+(a+a)')
+        mathExp.to_rpn()
+        self.assertEqual(mathExp.evaluate(a=a), 2 * a)
+
+    @given(a=st.lists(st.integers()),
+           b=st.lists(st.integers()),
+           c=st.lists(st.integers()))
+    def test_monoid_identity(self, a, b, c) -> None:
+        """
+        Associativity
+        For all a, b and c in S,
+        the equation (a + b) + c = a + (b + c) holds.
+        """
+        mathExp1 = MathExpByStrSub('(a + b) + c')
+        mathExp1.to_rpn()
+        mathExp2 = MathExpByStrSub('a + (b + c)')
+        mathExp2.to_rpn()
+
+        # (a + b) + c = a + (b + c)
+        self.assertEqual(mathExp1.evaluate(a=a, b=b, c=c),
+                         mathExp2.evaluate(a=a, b=b, c=c))
 
 
 if __name__ == '__main__':
